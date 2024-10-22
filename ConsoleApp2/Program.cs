e4caf9e60c9982c0458ab933574562aa6bf3cf75
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2024-10-22 11:14:38 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //периметр треугольника
+
+
+            //    Console.Write("a =");
+            //    int a = int.Parse(Console.ReadLine());
+            //    Console.Write("b =");
+            //    int b = int.Parse(Console.ReadLine());
+            //    Console.Write("c =");
+            //    int c = int.Parse(Console.ReadLine());
+
+            //    if (a < b + c && b < a + c && c < a + b)
+            //    {
+            //        double pp = (a + b + c) / 2;
+            //        double s = Math.Sqrt(pp*(pp-a)*(pp-b)*(pp-c));
+            //        Console.Write($"Треугольник существует\n{s:f2}");
+            //    }
+            //     else
+            //    {
+            //        Console.Write("Треугольника нет");
+            //    }
+            //    Console.Read();
+
+            //квадатные уравнения
+
+
+            Console.Write("a =");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b =");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("c =");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double d = Math.Pow(b,2)-4*a*c;
+            if (d>0)
+            {
+                double x1 = (-b + Math.Sqrt(d)) / 2 * a;
+                double x2 = (-b-Math.Sqrt(d)) / 2 * b;
+                Console.WriteLine($"{x1}{x2}");
+            }
+            else if (d==0)
+            {
+                double x1 = (-b + Math.Sqrt(d)) / 2*a;
+                Console.WriteLine($"{x1}");
+            }
+            else if (d<0)
+            {
+                Console.WriteLine("Нет корней");
+            }
+            Console.Read();
+        }
+    }
+}
